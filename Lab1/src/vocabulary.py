7ff66d61ac054e992572aafb05103527db5160d5
WDZRMPCBIT <WDZRMPCBIT@163.com>
2020-10-17 21:53:13 +0800

    rewrite vocabulary

--- Lab1/src/vocabulary.py
+++ Lab1/src/vocabulary.py
@@ -4,25 +4,80 @@ from copy import deepcopy
 
 class Vocabulary(object):
     def __init__(self, storage):
-        self.__words: List[str] = []
+        self.__vocabulary: List[Phrase] = []
         self.__storage = deepcopy(storage)
 
-    def add(self, word: str) -> None:
+    def add(self, words: List[str], occ: List[int]):
         """
-        将给定的单词插入单词表中
+        向词组表中添加一个词组
 
-        :param word: 待插入的单词
+        :param words: 要添加的词组
+        :param occ: 词组出现的次数
         """
-        if word in self.__words is not True:
-            self.__words.append(word)
-            self.__storage.add(word)
+        current = Phrase(words, occ)
+        if self.__storage.get(current.phrase()) == -1:
+            self.__storage.add(current.add(current.phrase()))
+            self.__vocabulary.append(current)
 
-    def get(self, word: str) -> int:
+    def get(self, words: List[str]) -> bool:
         """
-        查询给定词的哈希值，若词典中不存在则返回-1
+        从词组表中查询一个单词是否存在
+        若存在，则返回True
 
-        :param word: 待查询词
+        :param words: 待查询的单词
         """
-        if word in self.__words:
-            return self.__storage.get(word)
-        return -1
+        current = Phrase(words, 0)
+        if self.__storage.get(current.phrase()) != -1:
+            return True
+        return False
+
+    def load(self, path: str, storage):
+        """
+        从词典文件中加载词组
+        词典文件每一行的格式为：
+        词组单词个数 词组出现次数 单词1 单词2 ...
+
+        :param path: 词典文件路径
+        :param storage: 词典组织类
+        """
+        ret = Vocabulary(storage)
+        with open(path, 'r') as f:
+            for line in f:
+                items = line.split()
+                ret.add(items[2:], items[1])
+        return ret
+
+
+class Phrase(object):
+    def __init__(self, words: List[str], occ: int):
+        """
+        表示一个词组
+
+        :param words: 词组
+        :paran occ: 词组出现的次数，若为临时使用，请设为0
+        """
+        self.__words = deepcopy(words)
+        self.__occ = occ
+
+    def words(self):
+        """
+        将存储的词组以List[str]的形式返回
+        """
+        return deepcopy(self.__words)
+
+    def phrase(self):
+        """
+        将存储的词组以单个string的形式返回
+        """
+        ret: str = ""
+        for i in range(self.length()):
+            if i != 0:
+                ret = ret + " "
+            ret = ret + self.__words[i]
+        return ret
+
+    def occ(self):
+        return self.__occ
+
+    def length(self):
+        return len(self.__words)
